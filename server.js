fbe142a775fe968ad7e216d46fc49a3eef3125cd
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-22 13:03:23 -0400

    Creates GET and POST request handlers for the '/' endpoint using router and async/await to manage requests for actions in the database

--- server.js
+++ server.js
@@ -38,4 +38,28 @@ server.post('/', async (req, res) => {
     }
 });
 
+server.get('/', async (req, res) => {
+    try {
+        const actions = await db('actions');
+        res.status(200).json({ greeting: `${process.env.GREETING}`, actions});
+    } catch (error) {
+        console.log('\nERROR', error);
+        res.status(500).json({ error: 'Cannot retrieve the actions because Luis said you worry too much!' });
+    }
+});
+
+server.post('/', async (req, res) => {
+    try {
+        const [id] = await db('actions').insert(req.body);
+        const actions = await db('actions');
+
+        res.status(201).json(actions);
+    } catch (error) {
+        console.log('\nError', error);
+        res.status(500).json({ error: 'Cannot add the action; there is not enough swagger in it' });
+    }
+});
+
+
+
 module.exports = server;
