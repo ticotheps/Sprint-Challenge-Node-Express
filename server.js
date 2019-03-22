19f4a3a80b9efde7d7ece3e659a5acef8b12da62
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-22 14:33:23 -0400

    Migrates all of the request handlers from the 'actions-router.js' file into the 'projects-router.js' file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 
 const projectsRouter = require('./projects/projects-router.js');
-const actionsRouter = require('./actions/actions-router.js');
+// const actionsRouter = require('./actions/actions-router.js');
 
 const db = require('./data/dbConfig.js');
 
@@ -12,38 +12,39 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// routing
+
 server.use('/api/projects', projectsRouter);
-server.use('/api/actions', actionsRouter);
 
-server.get('/', (req, res) => {
-    res.send(`
-      <h1>Welcome to Tico's Node.js/Express Sprint Challenge API!</h1>
-      <h2>Don't worry. Be happy!</h2>
-      `);
-  });
+// server.get('/', (req, res) => {
+//     res.send(`
+//       <h1>Welcome to Tico's Node.js/Express Sprint Challenge API!</h1>
+//       <h2>Don't worry. Be happy!</h2>
+//       `);
+//   });
 
-// server.get('/', async (req, res) => {
-//     try {
-//         const projects = await db('projects');
-//         res.status(200).json({ greeting: `${process.env.GREETING}`, projects});
-//     } catch (error) {
-//         console.log('\nERROR', error);
-//         res.status(500).json({ error: 'Cannot retrieve the projects because Luis said you worry too much!' });
-//     }
-// });
+server.get('/', async (req, res) => {
+    try {
+        const projects = await db('projects');
+        res.status(200).json({ greeting: `${process.env.GREETING}`, projects});
+    } catch (error) {
+        console.log('\nERROR', error);
+        res.status(500).json({ error: 'Cannot retrieve the projects because Luis said you worry too much!' });
+    }
+});
 
-// server.post('/', async (req, res) => {
-//     try {
-//         const [id] = await db('projects').insert(req.body);
-//         const projects = await db('projects');
+server.post('/', async (req, res) => {
+    try {
+        const [id] = await db('projects').insert(req.body);
+        const projects = await db('projects');
 
-//         res.status(201).json(projects);
-//     } catch (error) {
-//         console.log('\nError', error);
-//         res.status(500).json({ error: 'Cannot add the project; there is not enough swagger in it' });
-//     }
-// });
+        res.status(201).json(projects);
+    } catch (error) {
+        console.log('\nError', error);
+        res.status(500).json({ error: 'Cannot add the project; there is not enough swagger in it' });
+    }
+});
+
+// server.use('/api/actions', actionsRouter);
 
 // server.get('/', async (req, res) => {
 //     try {
