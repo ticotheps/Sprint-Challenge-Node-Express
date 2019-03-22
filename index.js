2dc5427dd0d0b32f74c5df11387c1a6caf12287c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-22 11:52:09 -0400

    Creates 'server.js' file, imports all necessary middleware, invokes middleware, adds routing, creates endpoints for GET and POST requests (using async/await), and exports 'server'

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
-
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('require');
+const helmet = require('helmet');
+
+const projectsRouter = require('./projects/projects-router.js');
+
+const db = require('./data/dbConfig.js');
+
+const server = express();
+
+// middleware
+server.use(express.json());
+server.use(helmet());
+
+// routing
+server.use('/api/projects', projectsRouter);
+
+server.get('/', async (req, res) => {
+    try {
+        const projects = await db('projects');
+        res.status(200).json({ greeting: `${process.env.GREETING}`, projects});
+    } catch (error) {
+        console.log('\nERROR', error);
+        res.status(500).json({ error: 'Cannot retrieve the projects because Luis said you worry too much!' });
+    }
+});
+
+server.post('/', async (req, res) => {
+    try {
+        const [id] = await db('projects').insert(req.body);
+        const projects = await db('projects');
+
+        res.status(201).json(projects);
+    } catch (error) {
+        console.log('\nError', error);
+        res.status(500).json({ error: 'Cannot add the project; there is not enough swagger in it' });
+    }
+});
+
+module.exports = server;
