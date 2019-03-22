90bbe21acdad7a30c8e215d106acade6b27a2eb6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-22 12:32:43 -0400

    Creates a GET request handler with router and async/await to find a specific project by id and then return a list of actions that are associated with that project

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -39,4 +39,19 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+// Finds a specific project in the database by it's id and returns 
+// a list of the actions for that specific project from the 'http://localhost:5000/api/projects' url.
+router.get('/:id/actions', async (req, res) => {
+    try {
+        const actions = await Projects.getProjectActions(req.params.id);
+        
+            res.status(200).json(actions);
+    } catch (error) {
+        // logs error to the database
+        console.log(error);
+        res.status(500).json({ 
+            message: 'Error retrieving the actions for the specified project; perhaps you are not smiling enough'
+        });
+    }
+});
 
