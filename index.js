3d2f6de73da47dc88714199a6ce97686d5c7e0ae
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-22 11:59:37 -0400

    Imports dotenv module into index.js, defines 'port' and 'greeting' to interpolate values from .env file to make our listener more dynamic

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
+require('dotenv').config();
+const server = require('./server.js');
+
+const port = process.env.PORT || 4000;
+
+const greeting = process.env.GREETING;
+
+server.listen(port, () => {
+    console.log(`\n*** ${greeting}! Server is running http://localhost:${port} ***\n`);
+});
